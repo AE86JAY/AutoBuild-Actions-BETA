23e4a95cdfd0b013e05c5965c13625cda0dd8be8
ae86jay <236056971@163.com>
2025-03-15 07:11:33 +0800

    修改语法错误

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -226,11 +226,11 @@ EOF
 	
 	case "${OP_AUTHOR}/${OP_REPO}:${OP_BRANCH}" in
     immortalwrt/immortalwrt*)
-        WIFI_SCRIPT_PATH="package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
-# 设置国家代码为 CN
-sed -i "s|country='\${country || ''}'|country='CN'|g" ${WORK}/${WIFI_SCRIPT_PATH}
-# 根据频段设置不同 SSID
-sed -i "s|ssid='\${defaults?.ssid || \"ImmortalWrt\"}'|ssid='\${band_name == \"2g\" ? \"CandyTime_C9A700_2.4G\" : \"CandyTime_C9A700\"}'|g" ${WORK}/${WIFI_SCRIPT_PATH}
+WIFI_SCRIPT_PATH="package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
+    # 设置国家代码为 CN
+sed -i 's|country='\''${country || '\'''\''}'\''|country='\''CN'\''|g' "${WORK}/${WIFI_SCRIPT_PATH}"
+    # 根据频段设置不同 SSID
+sed -i 's|ssid='\''${defaults\?\.ssid || "ImmortalWrt"}'\''|ssid='\''${band_name == "2g" ? "CandyTime_C9A700_2.4G" : "CandyTime_C9A700"}'\''|g' "${WORK}/${WIFI_SCRIPT_PATH}"
     ;;
     esac
 }
